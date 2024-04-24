b1ddacc775a359ed8134cf06df6fe82a483e896a
Lalo <gonzalo.alvalon@gmail.com>
2024-04-24 13:14:33 +0200

    Add video file detection tests

--- src/files.rs
+++ src/files.rs
@@ -1,12 +1,24 @@
 use glob::{glob, Paths};
 use std::{collections::HashMap, iter::Chain};
 
-#[derive(Debug)]
+
+// TODO: Add serde support
+#[derive(Debug, PartialEq)]
 pub struct FolderAndFiles {
     pub folder: String,
     pub files: Vec<String>,
 }
 
+/// Retrieves a list of video file paths from the specified directory and its subdirectories.
+///
+/// # Arguments
+///
+/// * `path` - The path to the directory.
+///
+/// # Returns
+///
+/// A `FolderAndFiles` vector with the paths of all video files (with .mkv or .mp4 extension)
+/// found in the specified directory and its subdirectories.
 pub fn find_videos(path: &str) -> Vec<FolderAndFiles> {
     let path = path.trim_end_matches('/').trim_end_matches('\\');
 
@@ -35,3 +47,31 @@ fn organize_videos(videos_paths: Chain<Paths, Paths>) -> Vec<FolderAndFiles> {
     folders.into_iter().map(|(folder, files)| FolderAndFiles { folder, files }).collect()
 }
 
+// TODO: Normalize paths to use only forward slashes
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_find_videos() {
+        let path = "test/";
+        let expected_result = vec![
+            FolderAndFiles {
+                folder: "test\\files\\Mr Robot".to_string(),
+                files: vec![
+                    "test\\files\\Mr Robot\\MrRobot11.mkv".to_string(),
+                    "test\\files\\Mr Robot\\MrRobot102.mp4".to_string(),
+                ],
+            },
+            FolderAndFiles {
+                folder: "test\\files\\Silicon Valley\\S01".to_string(),
+                files: vec![
+                    "test\\files\\Silicon Valley\\S01\\SV0101.mkv".to_string(),
+                ],
+            },
+        ];
+
+        let result = find_videos(path);
+        assert_eq!(result, expected_result);
+    }
+}
