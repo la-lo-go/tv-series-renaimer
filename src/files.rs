d37488ad274bb97ab737ae015276b32d465c6d24
Lalo <gonzalo.alvalon@gmail.com>
2024-04-27 23:42:30 +0200

    Refactor file organization logic, add serialization support and normalized paths

--- src/files.rs
+++ src/files.rs
@@ -1,12 +1,11 @@
 use glob::{glob, Paths};
+use regex::Regex;
+use serde::Serialize;
 use std::{collections::HashMap, iter::Chain};
 
-
-// TODO: Add serde support
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Serialize)]
 pub struct FolderAndFiles {
-    pub folder: String,
-    pub files: Vec<String>,
+    pub entries: HashMap<String, Vec<String>>,
 }
 
 /// Retrieves a list of video file paths from the specified directory and its subdirectories.
@@ -19,7 +18,7 @@ pub struct FolderAndFiles {
 ///
 /// A `FolderAndFiles` vector with the paths of all video files (with .mkv or .mp4 extension)
 /// found in the specified directory and its subdirectories.
-pub fn find_videos(path: &str) -> Vec<FolderAndFiles> {
+pub fn find_videos(path: &str) -> FolderAndFiles {
     let path = path.trim_end_matches('/').trim_end_matches('\\');
 
     let videos_paths = glob(&format!("{}/**/*.mkv", path))
@@ -29,25 +28,29 @@ pub fn find_videos(path: &str) -> Vec<FolderAndFiles> {
     organize_videos(videos_paths)
 }
 
-fn organize_videos(videos_paths: Chain<Paths, Paths>) -> Vec<FolderAndFiles> {
+fn organize_videos(videos_paths: Chain<Paths, Paths>) -> FolderAndFiles {
     let mut folders: HashMap<String, Vec<String>> = HashMap::new();
+    let re = Regex::new(r#"\\+"#).unwrap();
 
     for video_path in videos_paths {
         if let Ok(video_path) = video_path {
             if let Some(folder) = video_path.parent().and_then(|p| p.to_str()) {
-                let entry = folders.entry(folder.to_string()).or_insert_with(Vec::new);
-                
+                let normalized_folder = re.replace_all(&folder, "/").to_string();
+                let entry = folders.entry(normalized_folder).or_insert_with(Vec::new);
+
                 if let Some(video_path_str) = video_path.to_str() {
-                    entry.push(video_path_str.to_string());
+                    let normalized_path = re.replace_all(&video_path_str, "/").to_string();
+                    entry.push(normalized_path);
                 }
             }
         }
     }
 
-    folders.into_iter().map(|(folder, files)| FolderAndFiles { folder, files }).collect()
+    FolderAndFiles {
+        entries: folders,
+    }
 }
 
-// TODO: Normalize paths to use only forward slashes
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -55,21 +58,23 @@ mod tests {
     #[test]
     fn test_find_videos() {
         let path = "test/";
-        let expected_result = vec![
-            FolderAndFiles {
-                folder: "test\\files\\Mr Robot".to_string(),
-                files: vec![
-                    "test\\files\\Mr Robot\\MrRobot11.mkv".to_string(),
-                    "test\\files\\Mr Robot\\MrRobot102.mp4".to_string(),
-                ],
+        let expected_result = FolderAndFiles {
+            entries: {
+                let mut folder_files = HashMap::new();
+                folder_files.insert(
+                    "test/files/Mr Robot".to_string(),
+                    vec![
+                        "test/files/Mr Robot/MrRobot11.mkv".to_string(),
+                        "test/files/Mr Robot/MrRobot102.mp4".to_string(),
+                    ],
+                );
+                folder_files.insert(
+                    "test/files/Silicon Valley/S01".to_string(),
+                    vec!["test/files/Silicon Valley/S01/SV0101.mkv".to_string()],
+                );
+                folder_files
             },
-            FolderAndFiles {
-                folder: "test\\files\\Silicon Valley\\S01".to_string(),
-                files: vec![
-                    "test\\files\\Silicon Valley\\S01\\SV0101.mkv".to_string(),
-                ],
-            },
-        ];
+        };
 
         let result = find_videos(path);
         assert_eq!(result, expected_result);
